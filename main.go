3467d3d2dd14d0126d29d1e38e24c17ce5b27fa7
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-08-18 17:13:49 -0300

    feat: sum route

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
+	e.GET("/soma/:x/:y", Sum)
 	e.Logger.Print("Listening on port 8080")
 	e.Logger.Fatal(e.Start(":8080"))
 }
